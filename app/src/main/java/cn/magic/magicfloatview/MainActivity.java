60f0f96b3edfc5506fc5ce36a6e0ffdc71a8acc7
yan <yanbobersky@gmail.com>
2017-01-01 18:20:21 +0800

    fly view is ok, new sky view start.

--- app/src/main/java/cn/magic/magicfloatview/MainActivity.java
+++ app/src/main/java/cn/magic/magicfloatview/MainActivity.java
@@ -5,33 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import cn.magic.library.flying.MagicFlyingView;
-import cn.magic.library.gift.MagicGiftLinearLayout;
+import cn.magic.library.MagicFlyLinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private MagicGiftLinearLayout giftLinearLayout;
-    private MagicFlyingView magicFlyingView;
+    private MagicFlyLinearLayout giftLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        giftLinearLayout = (MagicGiftLinearLayout) this.findViewById(R.id.gift_layout);
-        magicFlyingView = (MagicFlyingView) this.findViewById(R.id.flying_view);
-        magicFlyingView.addDrawable(R.drawable.favourite_love_blue);
-        magicFlyingView.addDrawable(R.drawable.favourite_love_pink);
-        magicFlyingView.addDrawable(R.drawable.favourite_love_red);
-        magicFlyingView.addDrawable(R.drawable.favourite_love_yellow);
+        giftLinearLayout = (MagicFlyLinearLayout) this.findViewById(R.id.gift_layout);
+        giftLinearLayout.addDrawable(R.drawable.favourite_love_blue);
+        giftLinearLayout.addDrawable(R.drawable.favourite_love_pink);
+        giftLinearLayout.addDrawable(R.drawable.favourite_love_red);
+        giftLinearLayout.addDrawable(R.drawable.favourite_love_yellow);
 
         ((Button)(this.findViewById(R.id.btn))).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (magicFlyingView.isAutoFlying()) {
-                    magicFlyingView.flyingAutoStop();
-                } else {
-                    magicFlyingView.flyingAutoStart();
-                }
+                giftLinearLayout.flying();
             }
         });
     }
